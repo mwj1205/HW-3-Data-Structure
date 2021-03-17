2e7ea1805753881b2eccde3ed7b71dd4d0c61a00
mwj1205 <mwj1205@naver.com>
2021-03-17 15:54:37 +0900

    Update ap2.c

--- ap2.c
+++ ap2.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(){
-    
+    int list[5];
+    int *plist[5];
+
+    plist[0] = (int *)malloc(sizeof(int));
+
+    printf("list[0] \t= %d\n", list[0]);
+    printf("address of list \t= %p\n",list);
+    printf("address of list[0] \t= %p\n",&list[0]);
+    printf("address of list + 0 \t= %p\n", list+0);
+    printf("address of list + 1 \t= %p\n", list+1);
+    printf("address of list + 2 \t= %p\n", list+2);
+    printf("address of list + 3 \t= %p\n", list+3);
+    printf("address of list + 4 \t= %p\n", list+4);
+    printf("address of list[4] \t= %p\n",&list[4]);
 }
