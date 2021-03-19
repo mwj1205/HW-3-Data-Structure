5f9011d26a0a64aab61171a060826273c4e4254e
mwj1205 <mwj1205@naver.com>
2021-03-19 11:32:49 +0900

    HW#3

--- p2-1.c
+++ p2-1.c
@@ -1,3 +1,5 @@
+﻿#include <stdio.h>
+
 #define MAX_SIZE 100
 
 float sum(float list[], int);
@@ -5,17 +7,23 @@ float input[MAX_SIZE], answer;
 int i;
 
 void main(void){
-    for(i=0; i < MAX_SIZE; i++)
+    for(i=0; i < MAX_SIZE; i++) // 배열에 0~99 입력
         input[i] = i;
-    answer = sum(input, MAX_SIZE);
+
+    printf("address of input = %p\n", input);
+
+    answer = sum(input, MAX_SIZE); // input 배열의 주소값을 넘겨준다
     printf("The sum is: %f\n", answer);
 }
 
-float sum(float list[], int n){
+float sum(float list[], int n){ // Call by reference. 배열의 주소를 받음
     int i;
     float tempsum = 0;
 
-    for(i = 0; i < n; i++)
+    printf("value of list = %p\n", list); // list는 input의 주소를 넘겨받는다.
+    printf("address of list= %p\n",&list); // list의 주소값이 출력된다. 
+
+    for(i = 0; i < n; i++)    // 함수에 모든 원소 더하기
         tempsum += list[i];
-    return tempsum;
+    return tempsum; // 더한 값 리턴
 }
--- p2-2.c
+++ p2-2.c
@@ -1,25 +1,29 @@
-#include <stdio.h>
+﻿#include <stdio.h>
 
 void print1 (int *ptr, int rows);
 
 int main(){
     int one[] = {0,1,2,3,4};
 
+    /* 배열의 이름은 배열의 첫번째 원소의 주소값을 가리키는 포인터이다. */
     printf("one     = %p\n", one);
     printf("&one    = %p\n", &one);
     printf("&one[0] = %p\n", &one[0]);
     printf("\n");
 
-    print1(&one[0],5);
+    print1(&one[0],5); // 배열의 첫 번째 원소의 주소값을 넘겨준다.
 
     return 0;
 }
 
-void print1 (int *ptr, int rows)
+void print1 (int *ptr, int rows) // 주소값과 원소 수를 넘겨받음
 {/* print out a one-dimensional array using a pointer */
     int i;
+
     printf ("Address Contents\n");
-    for (i = 0; i < rows; i++)
+
+    for (i = 0; i < rows; i++)  
         printf("%8u%5d\n", ptr + i, *(ptr + i));
+         // int형의 배열이기 때문에 int형의 사이즈 만큼 주소값이 차이난다
     printf("\n");
 }
